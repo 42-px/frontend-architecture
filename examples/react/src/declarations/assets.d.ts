fe0becb8c2e9020942529ec32e27c82e3e1666d6
Alexey Avramenko <2822698@gmail.com>
2020-05-14 05:05:48 +0700

    auth flow

--- examples/react/src/declarations/assets.d.ts
+++ examples/react/src/declarations/assets.d.ts
@@ -1,4 +1,4 @@
-declare module "*.svg" {
-  const content: React.FC<React.SVGAttributes<SVGElement>>;
-  export default content;
+declare module '*.svg' {
+  const content: React.FC<React.SVGAttributes<SVGElement>>
+  export default content
 }
